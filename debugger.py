d00b7d0649517a02a0477767cf49d700ca9aba17
PidgeyL <pieterjan.moreels@gmail.com>
2016-08-19 15:28:35 +0200

    make debug easier and safer

--- debugger.py
+++ debugger.py
@@ -12,19 +12,32 @@
 
 # Imports
 import sys
+import json
+from bson import json_util
 
 from main import *
+
+from TIME.lib.Toolkit import *
+
+def debug(obj):
+  text = json.dumps(to_dict(obj), indent=2, default=json_util.default)
+  open("TIME_debug.log", "a").write(text)
 
 if __name__ == '__main__':
   print("Type 'help' for more info")
   while True:
     try:
       data = input("> ")
+      if data.startswith("."):
+        data = "exec " + data[1:]
       if data.lower().startswith("exec "):
         data = data.split(maxsplit=1)
         payload = data[1] if len(data) > 1 else None
         if payload:
-          exec(payload)
+          try:
+            exec(payload)
+          except Exception as e:
+            print(e)
       else:
         if not interpretCommand(data):
           print("Unknown command. Press type help for help.")
