0e18f4f658249365eee405534f5cf98320057d4c
PidgeyL <pieterjan.moreels@gmail.com>
2016-09-02 19:38:32 +0200

    initial workings on quick_case

--- TIME/web/static/js/custom/scripts.js
+++ TIME/web/static/js/custom/scripts.js
@@ -2,3 +2,9 @@ function loading(target, page, query){
   $("body").css("cursor", "progress");
   $(target).load(page, query, function(){$("body").css("cursor", "default");});
 }
+
+$(function() {
+  $("[data-toggle='toggle']").click(function() {
+    $($(this).data("target")).toggleClass('in');
+  });
+});
