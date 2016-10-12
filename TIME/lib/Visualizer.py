50614de58111ca63f41368aca411d9afca564985
PidgeyL <pieterjan.moreels@gmail.com>
2016-10-12 15:20:21 +0200

    add backend code for front-end changes

--- TIME/lib/Visualizer.py
+++ TIME/lib/Visualizer.py
@@ -21,6 +21,8 @@ from flask import Flask, render_template
 from TIME.lib.Config        import Configuration as conf
 from TIME.lib.PluginManager import PluginManager as PM
 
+import TIME.lib.DatabaseLayer as db
+
 class Visualizer():
   @classmethod
   def generate_map(self, case):
@@ -39,6 +41,11 @@ class Visualizer():
     plugins = sorted(PM.get_all_plugins(), key=lambda k: k['name'])
     plugins = [x for x in plugins if x["name"] in [n.plugin for n in case.nodes]]
     labels = list(set([n.label.title() for n in case.nodes]))
+    access = [x['team_id'] for x in db.get_access_rights_for_case(case)]
+    teams  = []
+    for team in sorted(db.get_teams(), key=lambda k: k['name']):
+      team['allowed'] = True if team['team_id'] in access else False
+      teams.append(team)
     if not case.notes:       case.notes = ""
     if not case.description: case.description = ""
     for node in case.nodes:
@@ -46,4 +53,4 @@ class Visualizer():
       node.info = markdown.markdown("\n".join(["# %s\n%s"%(x, node.info[x])
                                               for x in sorted(node.info.keys()) if node.info[x]]))
     intel = [x.lower() for x in dir(conf) if x.startswith("INTEL_")]
-    return {'case': case, 'plugins': plugins, 'labels': labels, 'intel': intel}
+    return {'case': case, 'plugins': plugins, 'labels': labels, 'intel': intel, 'teams': teams}
